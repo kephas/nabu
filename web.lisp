6003856c0ca913a4fa9d66dec789b76f4a8b84cb
Pierre Thierry <pierre@nothos.net>
2014-12-17 20:43:14 +0100

    Activate Bootstrap tooltips

--- web.lisp
+++ web.lisp
@@ -58,6 +58,7 @@
 	,@body
 	(:script :src "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js")
 	(:script :src "/static/js/bootstrap.min.js")
+	(:script :src "/static/js/local.js")
 	(:script :src "/static/js/sticky-tabs.js"))))))
 
 (defroute "/" ()
